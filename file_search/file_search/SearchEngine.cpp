a427e0f118ac3346e521f06586a80e7f58c47a5c
AndreyRechkov <45733125+AndreyRechkov@users.noreply.github.com>
2019-01-06 15:56:16 +0300

    Add files via upload

--- file_search/file_search/SearchEngine.cpp
+++ file_search/file_search/SearchEngine.cpp
@@ -5,7 +5,6 @@
 #include <Windows.h>
 #include "FileInformation.h"
 
-
 SearchEngine::SearchEngine()
 {
 }
@@ -16,31 +15,37 @@ SearchEngine::~SearchEngine()
 }
 bool SearchEngine::Search(std::list<FileInformation> &Out) { return false; }
 
-void SearchEngine::SearchDirectory(std::string PathRoot,std::list<FileInformation> &Out)
+void SearchEngine::SearchDirectory(std::string PathRoot, std::list<FileInformation> &Out)
 {
 	HANDLE hFind;
+	std::string data;
 	WIN32_FIND_DATAA PathRoot2;
 	hFind = FindFirstFileA(PathRoot.c_str(), &PathRoot2);
 	PathRoot.erase(PathRoot.find('*'));
-	if (hFind != INVALID_HANDLE_VALUE)
-	{
+	//PathRoot.append(PathRoot2.cFileName);
+	if (hFind == INVALID_HANDLE_VALUE) {
+		std::cout << ("Invalid file handle. Error %d \n", GetLastError());
+
+	}
+	else {
+
 		do
 		{
+			if (strcmp(PathRoot2.cFileName, ".") == 0 || strcmp(PathRoot2.cFileName, "..") == 0) continue;//ïðîïóñê ññûëîê íà ñåáÿ è ðîäèòåëÿ
+			if (PathRoot2.dwFileAttributes & FILE_ATTRIBUTE_SYSTEM) { continue; }
 
-			if (PathRoot2.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY) {
-				
-				//ÐÅØÈÒÜ ÂÎÏÐÎÑ ÏÎ ÏÎÂÎÄÓ ÎÒÊÐÛÒÈß ÄÈÐÅÊÒÎÐÈÉ ÂËÎÆÅÍÍÛÕ
-				
-				
+			if (PathRoot2.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+				//while (PathRoot2.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+				//SearchDirectory(PathRoot, Out);//??????????????
+				strcat_s(PathRoot2.cFileName, "\\");//Add in Directory "\\" 
+				//}
 			}
+
 			FileInformation Example(PathRoot + PathRoot2.cFileName);
 			Out.push_back(Example);
-				
-		} while (FindNextFileA(hFind, &PathRoot2));
-		
-		FindClose(hFind);
-		//std::cout << data;
-	}
 
+		} while (FindNextFileA(hFind, &PathRoot2));
+		FindClose(hFind);
+	}
 }
- 
+
